c6b279822698fe2fe59f00e5b1e9eb7e0f1498ec
Idrees Hassan <idrees@idreesinc.com>
2017-05-19 12:24:28 -0400

    Added email notification when button is pressed.
    Added email notifications that are sent when the button is pressed.

--- Poof.js
+++ Poof.js
@@ -1,13 +1,66 @@
 var fs = require("fs");
 var dash_button = require('node-dash-button');
+var nodemailer = require('nodemailer');
+
 
 var config = JSON.parse(fs.readFileSync(__dirname + '/config.json', 'utf8'));
+var transporter;
 var dash = dash_button(config.mac_address, null, null, 'udp');
 
-console.log("Poof activated @ " + getTime());
-dash.on("detected", function (){
-	log("Button pressed!");
-});
+init();
+
+/**
+ * Initializes the program.
+ */
+ function init() {
+ 	dash.on("detected", function (){
+ 		log("Button pressed!");
+ 		notify("Button pressed!");
+ 	});
+
+ 	transporter = nodemailer.createTransport({
+ 		host: 'smtp.gmail.com',
+ 		port: 465,
+ 		secure: true,
+ 		auth: {
+ 			user: config.gmail,
+ 			pass: config.gmail_password
+ 		}
+ 	});
+ 	console.log("Poof activated @ " + getTime());
+ }
+
+/**
+ * Sends an email to the "email_to_notify" set in the config with the given message.
+ * @param  {String} message The message to send
+ */
+ function notify(message) {
+ 	sendEmail(config.email_to_notify, "🛎 " + getTime(), message);
+ }
+
+/**
+ * Sends an email with the given parameters.
+ * @param  {String}  to      The email address of the person to send the email to
+ * @param  {String}  subject The subject of the email
+ * @param  {String}  message The body of the email in plain-text
+ * @param  {Boolean} [mute]  Whether or not to mute the console output (Default is false)
+ */
+ function sendEmail(to, subject, message, mute) {
+ 	var options = {
+ 		from: '"Poof" <' + config.gmail + '>',
+ 		to: to,
+ 		subject: subject,
+ 		text: message
+ 	};
+ 	transporter.sendMail(options, function(error, info) {
+ 		if(error){
+ 			log("Error sending email:", error);
+ 			return;
+ 		} else if ((mute !== undefined && !mute) || mute === undefined) {
+ 			log("Email sent to " + to + ": [" + subject + "] " + options.text);
+ 		}
+ 	});
+ }
 
 /**
  * Prints output to console along with timestamp.
